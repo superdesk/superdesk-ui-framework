3fe6da915d758277174dd731d9eee4631471609c
Tomas Kikutis <t.kikutis@gmail.com>
2018-09-10 17:13:51 +0300

    Allow exporting stuff

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ const config = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
+        libraryTarget: 'umd',
         chunkFilename: '[name].chunk.js'
     },
 
