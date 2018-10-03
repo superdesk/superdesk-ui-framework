2cd60b071e484aaa0a062807434f23bb8aacd221
Tomas Kikutis <t.kikutis@gmail.com>
2018-10-03 14:02:58 +0300

    Merge pull request #256 from tomaskikutis/remove-unused-code

--- app/react-ui/index.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-export {Modal} from './Modal';
-export {ModalWithForm} from './ModalWithForm/index'
-export {Toggle} from './Toggle';
-export {ToggleBox} from './ToggleBox';
-export {Checkbox} from './Checkbox/index'
-export {Tabs, Tab, TabContent} from './Tabs/index'
