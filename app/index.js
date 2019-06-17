7f3eb393ca5e45d931e763a569a7c02c697227dd
Nemanja Pavlović <me@pavlovicnemanja.com>
2019-06-17 17:49:01 +0200

    fix(lint): Fixed lint issues

--- app/index.js
+++ app/index.js
@@ -1,18 +1,18 @@
 import './scripts';
 import './styles';
 
-export { reactToAngular1 } from './scripts/helpers/react-to-angular-1';
-export { ToggleBoxNext } from './scripts/toggleBoxNext';
-export { Positioner } from './scripts/positioner';
+export {reactToAngular1} from './scripts/helpers/react-to-angular-1';
+export {ToggleBoxNext} from './scripts/toggleBoxNext';
+export {Positioner} from './scripts/positioner';
 
-import { Input } from './components/Form/Input';
-import { Label } from './components/Form/Label';
-import { LineInput } from './components/Form/LineInput';
-import { Checkbox } from './components/Form/Checkbox';
-import { Select } from './components/Form/Select';
-import { TextArea } from './components/Form/TextArea';
-import { TextInput } from './components/Form/TextInput';
-import { TextAreaInput } from './components/Form/TextAreaInput';
+import {Input} from './components/Form/Input';
+import {Label} from './components/Form/Label';
+import {LineInput} from './components/Form/LineInput';
+import {Checkbox} from './components/Form/Checkbox';
+import {Select} from './components/Form/Select';
+import {TextArea} from './components/Form/TextArea';
+import {TextInput} from './components/Form/TextInput';
+import {TextAreaInput} from './components/Form/TextAreaInput';
 
 export const Form = {
     Label,
@@ -22,7 +22,7 @@ export const Form = {
     Select,
     TextArea,
     TextInput,
-    TextAreaInput
+    TextAreaInput,
 };
 
 export default angular.module('superdesk-ui', [
