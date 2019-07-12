a9280090c76087427267bf40aebbc2baf60367d8
Tomas Kikutis <t.kikutis@gmail.com>
2019-07-12 11:28:57 +0200

    add react types

--- app/scripts/HelloWorld.tsx
+++ app/scripts/HelloWorld.tsx
@@ -9,7 +9,6 @@ export class HelloWorld extends React.Component<IProps> {
         return (
             <div>
                 <span>{this.props.message}</span>
-                <span>{this.props.there_is_no_such_property_and_build_should_fail}</span>
             </div>
         );
     }
