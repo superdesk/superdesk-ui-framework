4707fa82b294e587e9453faead3420231d9e34ac
dragana <dragana_zivkovic@rocketmail.com>
2019-11-05 16:31:43 +0100

    feat(react): added buttons, labels and badges components

---
+++ app-typescript/components/Badge.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+interface IProps {
+    text: string;
+    types: string;
+    square: boolean;
+    
+}
+
+export class Labels extends React.Component<IProps> {
+    render() {
+    	let classes = ['badge'];
+        this.props.types ? classes.push('badge--' + this.props.types) : null
+        this.props.square ? classes.push('badge--square') : null
+
+        return (
+            <div>
+                 <span className = {classes.join(' ')}>{this.props.text}</span>
+            </div>
+        );
+    }
+}
