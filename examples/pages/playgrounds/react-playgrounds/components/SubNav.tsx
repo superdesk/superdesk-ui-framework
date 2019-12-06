094fd4e2ec9fd390f399a88d9017406784176518
dragana <dragana_zivkovic@rocketmail.com>
2019-12-06 11:08:09 +0100

    feat(react): theme props fixed

--- examples/pages/playgrounds/react-playgrounds/components/SubNav.tsx
+++ examples/pages/playgrounds/react-playgrounds/components/SubNav.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 
 interface IProps {
-    theme?: 'lighter' | 'darker';
+    theme?: 'light' | 'dark';
     children?: React.ReactNode;
 }
 
