3cda24a286bb56de3b9f8fb32fd4723496990865
dragana <dragana_zivkovic@rocketmail.com>
2019-12-23 11:38:12 +0100

    feat(react): fixed labels

--- app-typescript/components/Label.tsx
+++ app-typescript/components/Label.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import classNames from 'classnames';
-
 interface IProps {
     text: string;
     type?: 'default' | 'primary' | 'success' | 'warning' | 'alert' | 'highlight' | 'sd-green';
@@ -10,24 +9,14 @@ interface IProps {
     noTransform?: boolean;
     style?: 'filled' | 'hollow'; // defaults to 'filled'
 }
-
 export class Label extends React.PureComponent<IProps> {
     render() {
-        let defaultValueType = '';
-        if (this.props.type === undefined) {
-            defaultValueType = 'default'
-        }
         let classes = classNames('label', {
             [`label--${this.props.size}`]: this.props.size !== 'normal' && this.props.size !== undefined,
             'label--no-transform': this.props.noTransform,
             [`label--${this.props.type}`]: this.props.type !== undefined && !this.props.color,
-<<<<<<< HEAD
             [`${this.props.color}`]: this.props.color !== undefined && !this.props.type && !this.props.style,
             'label--hollow': this.props.style === 'hollow',
-=======
-            [`${this.props.color}`]: this.props.color !== undefined && !this.props.type,
-            'label--hollow': this.props.style === 'hollow' && defaultValueType || this.props.style === 'hollow',
->>>>>>> feat(react): fixed hollow issue with label color
             [`hollow-${this.props.color}`]: this.props.color && this.props.style === 'hollow',
         });
         if (this.props.onClick) {
