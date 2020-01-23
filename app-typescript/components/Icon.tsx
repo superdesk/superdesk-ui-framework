cbc1a8436212d199fbb6a7570e5fd743b7cd20e9
dragana <dragana_zivkovic@rocketmail.com>
2020-01-23 11:29:08 +0100

    feat(react): added NotificationToast component

--- app-typescript/components/Icon.tsx
+++ app-typescript/components/Icon.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import classNames from 'classnames';
-
 interface IProps {
     name?: string;
     size?: 'small' | 'big'; // defaults to 'small'
@@ -11,7 +10,7 @@ export class Icon extends React.PureComponent<IProps> {
     render() {
         let classes = classNames({
             [`icon-${this.props.name}`]:
-            (this.props.name && !this.props.size) || (this.props.name && this.props.size === 'small'),
+                (this.props.name && !this.props.size) || (this.props.name && this.props.size === 'small'),
             [`big-icon--${this.props.name}`]: this.props.name && this.props.size === 'big',
             [`${this.props.type}`]: this.props.type,
         });
