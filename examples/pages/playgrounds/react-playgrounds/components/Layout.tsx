e526e16edd3c98c46c1da8e4d6d9e9995436aeac
dragana <dragana_zivkovic@rocketmail.com>
2020-02-18 09:38:17 +0100

    react(feat): updated Layout component

--- examples/pages/playgrounds/react-playgrounds/components/Layout.tsx
+++ examples/pages/playgrounds/react-playgrounds/components/Layout.tsx
@@ -10,46 +10,45 @@ interface IProps {
     theme?: string;
 }
 
-export class Layout extends React.PureComponent<IProps> {
-    componentDidMount(){
-        let element = document.getElementById('1');
-        let attr = document.createAttribute('theme');
-        attr.value = this.props.theme;
-        element.setAttributeNode(attr);
+export const Layout = ({
+    header,
+    children,
+    openA,
+    theme,
+}: IProps) => {
+    function UpdateTheme() {
+        React.useEffect(() => {
+            let element = document.getElementById('1');
+            let attr = document.createAttribute('theme');
+            attr.value = theme;
+            element.setAttributeNode(attr);
+        }, [theme])
     }
-    
-    componentDidUpdate() {
-        let element = document.getElementById('1');
-        let attr = document.createAttribute('theme');
-        attr.value = this.props.theme;
-        element.setAttributeNode(attr);
-    }
-    render() {
-        let classes = classNames('sd-content sd-content-wrapper', {
-            [`${this.props.theme}`]: this.props.theme,
-        })
-        return (
-            <div className='sd-page-grid--test docs-page__full-width-helper'>
-                <div className='sd-main-menu'>
-                    <div className='sd-main-menu__inner'></div>
-                </div>
-                <header className='sd-top-menu'>
-                    <a className='sd-top-menu__collapse-nav'>
-                        <i className='icon-collapse icon--white'></i>
-                    </a>
-                    <p className='sd-top-menu__header'>{this.props.header}</p>
-                </header>
-                <section id='1' className={classes}>
-                    <SidebarMenu />
-                    <div id='leftContent' className='sd-content-wrapper__main-content-area sd-main-content-grid comfort'>
-                        {this.props.children}
-                    </div>
-                    <AuthoringContent open={this.props.openA} />
-                </section>
-                <footer className='bottom-bar'>
-                    This is the footer.
-                </footer>
+
+    let classes = classNames('sd-content sd-content-wrapper', {
+        [`${theme}`]: theme,
+    });
+    return (
+        <div className='sd-page-grid--test docs-page__full-width-helper'>
+            <div className='sd-main-menu'>
+                <div className='sd-main-menu__inner'></div>
             </div>
+            <header className='sd-top-menu'>
+                <a className='sd-top-menu__collapse-nav'>
+                    <i className='icon-collapse icon--white'></i>
+                </a>
+                <p className='sd-top-menu__header'>{header}</p>
+            </header>
+            <section id='1' className={classes}>
+                <SidebarMenu />
+                <div id='leftContent' className='sd-content-wrapper__main-content-area sd-main-content-grid comfort'>
+                    {children}
+                </div>
+                <AuthoringContent open={openA} />
+            </section>
+            <footer className='bottom-bar'>
+                This is the footer.
+            </footer>
+        </div>
         );
-    }
 }
