38c9dfd8666141d42ea10eb0878339ebcc7798d9
Nemanja Pavlovic <me@pavlovicnemanja.com>
2020-03-10 16:10:30 +0100

    fix lint

--- app-typescript/components/Input.tsx
+++ app-typescript/components/Input.tsx
@@ -24,8 +24,8 @@ export class Input extends React.Component<IProps, IState> {
 
         this.state = {
             value: this.props.value ?? '',
-            invalid: this.props.invalid ?? false
-        }
+            invalid: this.props.invalid ?? false,
+        };
 
         this.handleChange = this.handleChange.bind(this);
     }
@@ -36,27 +36,36 @@ export class Input extends React.Component<IProps, IState> {
 
         if (this.props.maxLength && !this.props.invalid) {
             this.setState({invalid: event.target.value.length > this.props.maxLength});
-        }    
+        }
     }
 
     render() {
         const classes = classNames('sd-input', {
             'sd-input--required': this.props.required,
             'sd-input--disabled': this.props.disabled,
-            'sd-input--invalid': this.props.invalid || this.state.invalid
+            'sd-input--invalid': this.props.invalid || this.state.invalid,
         });
 
         return (
             <div className={classes}>
                 { this.props.label ? <label className='sd-input__label'>{this.props.label}</label> : null }
 
-                <input className='sd-input__input' type='text' value={this.state.value} onChange={this.handleChange} disabled={this.props.disabled} />
+                <input className='sd-input__input'
+                    type='text'
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    disabled={this.props.disabled} />
 
-                { this.props.maxLength ? <div className='sd-input__char-count'>{this.state.value.length} / {this.props.maxLength}</div> : null }
+                { this.props.maxLength ?
+                    <div className='sd-input__char-count'>{this.state.value.length} / {this.props.maxLength}</div>
+                    : null }
 
                 <div className='sd-input__message-box'>
-                    { this.props.info && !this.props.invalid && !this.state.invalid ? <div className='sd-input__hint'>{this.props.info}</div> : null }
-                    { this.props.invalid || this.state.invalid ? <div className='sd-input__message'>{this.props.error}</div> : null }
+                    { this.props.info && !this.props.invalid && !this.state.invalid ?
+                        <div className='sd-input__hint'>{this.props.info}</div> : null }
+                    { this.props.invalid || this.state.invalid ?
+                        <div className='sd-input__message'>{this.props.error}</div>
+                        : null }
                 </div>
             </div>
         );
