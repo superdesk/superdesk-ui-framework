63ed191b3c15f635467a86b0cd097610129e757a
dragana <dragana_zivkovic@rocketmail.com>
2020-03-26 14:55:06 +0100

    react(feat): updated alert

--- app-typescript/components/Alert.tsx
+++ app-typescript/components/Alert.tsx
@@ -43,7 +43,8 @@ export class Alert extends React.PureComponent<IProps, IState> {
             <div className='sd-alert__container'>
                 <div className={classesAlert}>
                     {this.props.restoreIcon ?
-                        <button className='sd-alert__close' onClick={this.onToggle} ></button> : null}
+                        <button className='sd-alert__close' onClick={this.onToggle} >
+                            <span className='visuallyhidden'>close</span></button> : null}
                     {this.props.children}
                 </div>
                 {this.props.restoreIcon ?
