e424b4c2b51b8a0112569bfd4023ddd6b84511f8
dragana <dragana_zivkovic@rocketmail.com>
2020-03-31 18:29:08 +0200

    react(feat): update change side show

--- app-typescript/components/Dropdown.tsx
+++ app-typescript/components/Dropdown.tsx
@@ -3,28 +3,38 @@ import classNames from 'classnames';
 interface IProps {
     name: string;
     align?: 'left' | 'right';
+    side?: 'left' | 'right';
 }
 export const Dropdown = ({
     name,
     align,
+    side,
 }: IProps) => {
     const [open, setOpen] = React.useState(false);
     const classes = classNames('dropdown', {
-        [`open`]: open,
+        ['open']: open,
         ['dropdown--align-right']: align === 'right',
+        [`dropdown--drop${side}`]: side,
     });
+
     function isOpen() {
-        setOpen(true);
-        document.addEventListener('click', closeMenu);
+        if (!open) {
+            setOpen(true);
+            document.addEventListener('click', closeMenu);
+        } else {
+            setOpen(false);
+        }
     }
 
     function closeMenu() {
         setOpen(false);
         document.removeEventListener('click', closeMenu);
     }
+
     return (
-        <div className={classes}>
-            <button className='dropdown__toggle dropdown-toggle' onClick={isOpen}>{name}</button>
+        <div className={classes} dropdown-append-to-body='true'>
+            <button className='dropdown__toggle dropdown-toggle' onClick={isOpen}>{name}
+                <span className="dropdown__caret"></span></button>
             <ul className='dropdown__menu'>
                 <li><button>one</button></li>
                 <li><button>two</button></li>
