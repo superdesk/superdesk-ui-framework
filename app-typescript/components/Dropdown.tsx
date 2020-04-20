74050e70a3d836d2dc9f8ad724f8dbe4ca73845e
dragana <dragana_zivkovic@rocketmail.com>
2020-04-20 21:42:26 +0200

    react(feat): component structure changed

--- app-typescript/components/Dropdown.tsx
+++ app-typescript/components/Dropdown.tsx
@@ -1,20 +1,47 @@
 import * as React from 'react';
 import classNames from 'classnames';
+import { PropsList } from './PropsList';
 interface IProps {
     name: string;
     align?: 'left' | 'right';
     side?: 'left' | 'right';
+    children: React.ReactNode;
 }
+
+interface IPropsItem {
+    text: string;
+    icon?: string;
+}
+
+interface IPropsLabel {
+    text: string;
+}
+
 export const Dropdown = ({
     name,
     align,
     side,
+    children,
 }: IProps) => {
     const [open, setOpen] = React.useState(false);
+    const [height, setHeight] = React.useState(false);
+    const ref = React.useRef(null);
+    React.useEffect(() => {
+        let number = getOffset(ref.current);
+        let second = getBodyOffset();
+        let heightEl = heightElement(ref.current);
+        if (heightEl > second - number.bottom) {
+            setHeight(true);
+        } else {
+            setHeight(false);
+        }
+    }, [open]);
+
     const classes = classNames('dropdown', {
         ['open']: open,
         ['dropdown--align-right']: align === 'right',
         [`dropdown--drop${side}`]: side,
+        ['dropdown--dropup']: height,
     });
 
     function isOpen() {
@@ -31,15 +58,53 @@ export const Dropdown = ({
         document.removeEventListener('click', closeMenu);
     }
 
+    function getOffset(el: any) {
+        const rect = el.getBoundingClientRect();
+        return {
+            left: rect.left + window.scrollX,
+            bottom: rect.bottom + window.scrollY,
+        };
+    }
+
+    function getBodyOffset() {
+        return screen.height;
+    }
+
+    function heightElement(el: any) {
+        return el.clientHeight;
+    }
+
     return (
-        <div className={classes} dropdown-append-to-body='true'>
+        <div className={classes} >
             <button className='dropdown__toggle dropdown-toggle' onClick={isOpen}>{name}
                 <span className="dropdown__caret"></span></button>
-            <ul className='dropdown__menu'>
-                <li><button>one</button></li>
-                <li><button>two</button></li>
-                <li><button>three</button></li>
+            <ul className='dropdown__menu' ref={ref}>
+                {children}
             </ul>
         </div>
+    );
+};
+
+export const DropdownItem = ({
+    text,
+    icon,
+}: IPropsItem) => {
+
+    return (
+        <li><button><i className={icon ? ('icon-' + icon) : ''}></i>{text}</button></li>
+    );
+};
+
+export const DropdownDivider = ({ }) => {
+    return (<li className="dropdown__menu-divider"></li>);
+};
+
+export const DropdownLabel = ({
+    text,
+}: IPropsLabel) => {
+    return (
+        <li>
+            <div className="dropdown__menu-label">{text}</div>
+        </li>
     );
 };
