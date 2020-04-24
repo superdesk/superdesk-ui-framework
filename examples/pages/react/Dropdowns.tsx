fd248c6ecb9ae2a856bf359561d1603f71ecf26b
dragana <dragana_zivkovic@rocketmail.com>
2020-04-24 14:55:16 +0200

    react(feat): added examples

--- examples/pages/react/Dropdowns.tsx
+++ examples/pages/react/Dropdowns.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as Markup from '../../js/react';
 import { Dropdown } from '../../../app-typescript';
+import { SubNav } from '../../../app-typescript';
 
 export default class DropdownDoc extends React.Component {
     render() {
@@ -83,11 +84,11 @@ export default class DropdownDoc extends React.Component {
                             <Dropdown.Item text='Copy' icon='copy' />
                             <Dropdown.Item text='Delete' icon='trash' />
                             <Dropdown.Label text='actions 2' />
-                            <Dropdown name='Second level actions' level={true} icon='star'>
+                            <Dropdown name='Second level actions' level={true} icon='star' align='left'>
                                 <Dropdown.Item text='Action 1' />
                                 <Dropdown.Item text='Action 2' />
                                 <Dropdown.Item text='Action 3' />
-                                <Dropdown.Item text='Action 3' />
+                                <Dropdown.Item text='Action 4' />
                             </Dropdown>
                         </Dropdown>
 
@@ -102,7 +103,7 @@ export default class DropdownDoc extends React.Component {
                                 <Dropdown.Item text='Action 1' />
                                 <Dropdown.Item text='Action 2' />
                                 <Dropdown.Item text='Action 3' />
-                                <Dropdown.Item text='Action 3' />
+                                <Dropdown.Item text='Action 4' />
                             </Dropdown>
                         </Dropdown>
                     </Markup.ReactMarkupPreview>
@@ -112,20 +113,27 @@ export default class DropdownDoc extends React.Component {
                     </Markup.ReactMarkupCode>
                 </Markup.ReactMarkup>
 
+                <h3 className="docs-page__h3">Navigation dropdown</h3>
                 <p className='docs-page__paragraph'></p>
                 <Markup.ReactMarkup>
                     <Markup.ReactMarkupPreview>
-                        <Dropdown name='test' icon='dots'>
-                            <Dropdown.Item text='one' />
-                            <Dropdown.Item text='two' icon='pencil' />
-                            <Dropdown.Item text='PROBA' />
-                            <Dropdown name='inside' level={true} icon='pencil'>
-                                <Dropdown.Item text='four' />
+                        <SubNav zIndex={1} color='darker'>
+                            <h3 className="subnav__page-title">Subnav title</h3>
+                            <Dropdown name='dots' icon='dots-vertical' navDropdown={true}>
+                                <Dropdown.Label text='actions' />
+                                <Dropdown.Divider />
+                                <Dropdown.Item text='Edit' icon='pencil' />
+                                <Dropdown.Item text='Copy' icon='copy' />
+                                <Dropdown.Item text='Delete' icon='trash' />
+                                <Dropdown.Label text='actions 2' />
+                                <Dropdown name='Second level actions' level={true} icon='star' align='left'>
+                                    <Dropdown.Item text='Action 1' />
+                                    <Dropdown.Item text='Action 2' />
+                                    <Dropdown.Item text='Action 3' />
+                                    <Dropdown.Item text='Action 4' />
+                                </Dropdown>
                             </Dropdown>
-                        </Dropdown>
-                        <Dropdown name='buttons' headerFooter={true}>
-                            <Dropdown.Item text='first' />
-                        </Dropdown>
+                        </SubNav>
                     </Markup.ReactMarkupPreview>
                     <Markup.ReactMarkupCode>{`
                         
@@ -133,6 +141,44 @@ export default class DropdownDoc extends React.Component {
                     </Markup.ReactMarkupCode>
                 </Markup.ReactMarkup>
 
+                <h3 className="docs-page__h3">Navigation dropdown with fixed header and footer</h3>
+                <p className='docs-page__paragraph'></p>
+                <Markup.ReactMarkup>
+                    <Markup.ReactMarkupPreview>
+                        <SubNav zIndex={1} color='darker'>
+                            <h3 className="subnav__page-title">Subnav title</h3>
+                            <Dropdown name='dots' icon='dots-vertical' navDropdown={true} headerFooter={true}>
+                                <Dropdown.Header title='header actions'>
+                                    <Dropdown.Item text='Header action one' icon='download' />
+                                    <Dropdown.Item text='Header action one' icon='trash' />
+                                </Dropdown.Header>
+                                <Dropdown.Body title='body actions'>
+                                    <Dropdown.Item text='Edit' icon='pencil' />
+                                    <Dropdown.Item text='Copy Item' icon='copy' />
+                                    <Dropdown.Item text='Action 2' icon='envelope' />
+                                    <Dropdown.Item text='Action 3' icon='heart' />
+                                    <Dropdown.Item text='Action 4' icon='print' />
+                                    <Dropdown.Item text='Action 5' icon='plus-sign' />
+                                    <Dropdown.Item text='Action 6' icon='minus-sign' />
+                                    <Dropdown.Item text='Action 7' icon='refresh' />
+                                    <Dropdown.Item text='Action 8' icon='pick' />
+                                    <Dropdown.Item text='Action 9' icon='bell' />
+                                    <Dropdown.Item text='Action 10' icon='kill' />
+                                    <Dropdown.Item text='Action 11' icon='settings' />
+                                    <Dropdown.Item text='Action 12' icon='cut' />
+                                </Dropdown.Body>
+                                <Dropdown.Footer title='footer actions'>
+                                    <Dropdown.Item text='Create gallery' icon='slideshow' />
+                                    <Dropdown.Item text='Create package' icon='composite' />
+                                </Dropdown.Footer>
+                            </Dropdown>
+                        </SubNav>
+                    </Markup.ReactMarkupPreview>
+                    <Markup.ReactMarkupCode>{`
+                        
+                    `}
+                    </Markup.ReactMarkupCode>
+                </Markup.ReactMarkup>
             </section>
 
         )
