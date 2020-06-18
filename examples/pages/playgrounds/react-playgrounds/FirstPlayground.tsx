32535e47121740600a5584aeb0184a3289774fbb
dragana <dragana_zivkovic@rocketmail.com>
2020-06-18 11:19:40 +0200

    updated to adjust to sams

--- examples/pages/playgrounds/react-playgrounds/FirstPlayground.tsx
+++ examples/pages/playgrounds/react-playgrounds/FirstPlayground.tsx
@@ -1,59 +1,115 @@
 import * as React from 'react';
 import * as Components from './components/Index';
+import { ButtonGroup, Button, NavButton, SubNav, Dropdown, CheckButtonGroup, RadioButton, Icon } from '../../../../app-typescript/index';
 import { Input } from '../../../../app-typescript/components/Input';
 import { Select, Option } from '../../../../app-typescript/components/Select';
-import { Button } from '../../../../app-typescript/components/Button';
 
 interface IProps {
     children?: React.ReactNode;
 }
 
 interface IState {
-    open: boolean;
     openA: boolean;
-    openF: boolean;
-    theme: string;
+    openFilter: boolean;
+    theme: 'dark' | 'light';
+    itemType: string;
+    dropDownState: string;
+    openPreview: boolean;
 }
 
 export class FirstPlayground extends React.Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
         this.state = {
-            open: false,
             openA: false,
-            openF: false,
+            openFilter: false,
             theme: 'light',
+            itemType: 'itemtype01',
+            dropDownState: '',
+            openPreview: false,
         }
-        this.handleClick = this.handleClick.bind(this);
         this.handleClickA = this.handleClickA.bind(this);
         this.handleFilter = this.handleFilter.bind(this);
-        this.sendTheme = this.sendTheme.bind(this);
-    }
-
-    handleClick(child: boolean) {
-        this.setState({ open: child })
+        this.handlePreview = this.handlePreview.bind(this);
+        this.handleTheme = this.handleTheme.bind(this);
     }
 
     handleClickA(child: boolean) {
         this.setState({ openA: child })
     }
 
-    handleFilter(child: boolean) {
-        this.setState({ openF: child })
+    handleFilter() {
+        this.setState((state) => ({
+            openFilter: !state.openFilter,
+        }));
     }
 
-    sendTheme(child: string) {
-        this.setState({ theme: child })
+    handlePreview() {
+        this.setState((state) => ({
+            openPreview: !state.openPreview,
+        }));
+    }
+
+    handleTheme() {
+        if (this.state.theme === 'light') {
+            this.setState({
+                theme: 'dark'
+            })
+        } else {
+            this.setState({
+                theme: 'light'
+            })
+        }
     }
 
     render() {
         return (
             <Components.Layout header='First playground' openA={this.state.openA} theme={this.state.theme}>
                 {/* TOOLBAR HEADER */}
-                <Components.ToolbarHeader />
+                <Components.ToolbarHeader >
+                    <SubNav theme={this.state.theme}>
+                        <ButtonGroup>
+                            <Dropdown
+                                items={[
+                                    {
+                                        type: 'group', label: 'Archives', items: [
+                                            'divider',
+                                            { label: 'All archives', onSelect: () => this.setState({ dropDownState: 'All archives' }) },
+                                            { label: 'Media archive', onSelect: () => this.setState({ dropDownState: 'Media archive' }) },
+                                            { label: 'File archive', onSelect: () => this.setState({ dropDownState: 'File archive' }) },
+                                            { label: 'AP images', onSelect: () => this.setState({ dropDownState: 'AP archive' }) },
+                                        ]
+                                    }]}>
+                                <NavButton onClick={() => false} />
+                            </Dropdown>
+                        </ButtonGroup>
+                        <ButtonGroup align='right'>
+                            <NavButton icon='dots-vertical' onClick={() => false} />
+                        </ButtonGroup>
+                    </SubNav>
+                    <SubNav theme={this.state.theme}>
+                        <ButtonGroup >
+                            <NavButton icon='filter-large' onClick={this.handleFilter} />
+                        </ButtonGroup>
+                        <CheckButtonGroup >
+                            <RadioButton value={this.state.itemType} onChange={(value) => this.setState({ itemType: value })} options={[
+                                { value: 'itemtype01', label: 'All item types' },
+                                { value: 'itemtype02', label: 'Images only' },
+                                { value: 'itemtype03', label: 'Videos only' },
+                                { value: 'itemtype04', label: 'Documents only' }]} />
+                        </CheckButtonGroup>
+                        <ButtonGroup align='right'>
+                            <NavButton icon='adjust' onClick={this.handleTheme} />
+                            <NavButton icon='th-list' onClick={() => false} />
+                        </ButtonGroup>
+                    </SubNav>
+                </Components.ToolbarHeader>
                 {/* FILTER PANEL*/}
-                <Components.FilterPanel openF={this.state.openF} >
+                <Components.FilterPanel openF={this.state.openFilter} >
                     <div className="side-panel__header side-panel__header--border-b">
+                        <a className="icn-btn side-panel__close" onClick={this.handleFilter}>
+                            <Icon name='close-small' />
+                        </a>
                         <h3 className="side-panel__heading">Advanced filters</h3>
                     </div>
                     <div className="side-panel__content">
@@ -144,15 +200,11 @@ export class FirstPlayground extends React.Component<IProps, IState> {
 
                 </Components.FilterPanel>
                 {/* MAIN CONTENT (Monitoring) */}
-                <Components.MainContent
-                    handleFilter={this.handleFilter}
-                    handleClick={this.handleClick}
-                    handleClickA={this.handleClickA}
-                    sendTheme={this.sendTheme}>
+                <Components.MainContent>
 
                 </Components.MainContent>
                 {/* PREVIEW PANEL*/}
-                <Components.PreviewPanel open={this.state.open} />
+                <Components.PreviewPanel open={this.state.openPreview} />
                 {/* OVERLAY PANEL (Send To) */}
                 <Components.OverlayPanel />
             </Components.Layout >
--- examples/pages/playgrounds/react-playgrounds/components/MainContent.tsx
+++ examples/pages/playgrounds/react-playgrounds/components/MainContent.tsx
@@ -2,63 +2,13 @@ import * as React from 'react';
 import {ThemeDropdown} from '../components/ThemeDropdown';
 interface IProps {
     children?: React.ReactNode;
-    handleClick(child): void;
-    handleClickA(child): void;
-    handleFilter(child): void;
-    sendTheme(child): void;
 }
 
-interface IState {
-    open: boolean;
-    openA: boolean;
-    openF: boolean;
-}
 
-export class MainContent extends React.PureComponent<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            open: true,
-            openA: true,
-            openF: true,
-        };
-        this.handleClick = this.handleClick.bind(this);
-        this.handleClickA = this.handleClickA.bind(this);
-        this.handleFilter = this.handleFilter.bind(this);
-    }
-
-    handleClick() {
-        this.setState((state) => ({
-            open: !state.open,
-        }));
-        this.props.handleClick(this.state.open);
-    }
-
-    handleClickA() {
-        this.setState((state) => ({
-            openA: !state.openA,
-        }));
-        this.props.handleClickA(this.state.openA);
-    }
-
-    handleFilter() {
-        this.setState((state) => ({
-            openF: !state.openF,
-        }));
-        this.props.handleFilter(this.state.openF);
-    }
-    
-    handleThemeParent = (childData) => {
-        this.props.sendTheme(childData);
-    }
-
+export class MainContent extends React.PureComponent<IProps> {
     render() { 
         return (
             <div className='sd-main-content-grid__content sd-padding--2'>
-                <button onClick={this.handleFilter}>Test filter</button>
-                <button onClick={this.handleClick}>Test preview</button>
-                <button onClick={this.handleClickA}>Test authoring</button>
-                <ThemeDropdown handleThemeParent = {this.handleThemeParent}/>
                 {this.props.children}
             </div>
         );
