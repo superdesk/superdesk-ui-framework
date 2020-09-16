b60390686c9b45a88fd38c2cb43ffc1f38c9cf84
dragana <dragana_zivkovic@rocketmail.com>
2020-09-16 18:25:56 +0200

    conflicts fix

--- app-typescript/index.ts
+++ app-typescript/index.ts
@@ -1,7 +1,6 @@
 // this intermediary file is required in order to be able to generate TypeScript definitions
 // all exports in this file must also be exported from ../app/index.js
 
-<<<<<<< HEAD
 export { HelloWorld } from './components/HelloWorld';
 export { Button } from './components/Button';
 export { Input } from './components/Input';
@@ -46,52 +45,7 @@ export { Dropdown } from './components/Dropdown';
 export { Tag } from './components/Tag';
 export { TabLabel, TabPanel, TabContent, Tabs } from './components/TabCustom';
 export { EmptyState } from './components/EmptyState';
-=======
-export {HelloWorld} from './components/HelloWorld';
-export {Button} from './components/Button';
-export {Input} from './components/Input';
-export {Select, Option} from './components/Select';
-export {Label} from './components/Label';
-export {Badge} from './components/Badge';
-export {Alert} from './components/Alert';
-export {AvatarWrapper} from './components/Avatar';
-export {AvatarContentText} from './components/Avatar';
-export {AvatarContentImage} from './components/Avatar';
-export {IconButton} from './components/IconButton';
-export {IconLabel} from './components/IconLabel';
-export {Tooltip} from './components/Tooltip';
-export {DatePicker} from './components/DatePicker';
-export {DatePickerISO} from './components/DatePicker';
-export {TimePicker} from './components/TimePicker';
-export {FormLabel} from './components/FormLabel';
-export {Switch} from './components/Switch';
-export {ButtonGroup} from './components/ButtonGroup';
-export {Loader} from './components/Loader';
-export {Radio} from './components/Radio';
-export {Checkbox} from './components/Checkbox';
-export {RadioButton} from './components/RadioButton';
-export {CheckboxButton} from './components/CheckboxButton';
-export {CheckGroup} from './components/CheckGroup';
-export {CheckButtonGroup} from './components/CheckButtonGroup';
-export {NavButton} from './components/NavButton';
-export {Tab, TabList} from './components/TabList';
-export {LeftMenu} from './components/LeftMenu';
-export {SubNav} from './components/SubNav';
-export {SlidingToolbar}  from './components/SlidingToolbar';
-export {StrechBar}  from './components/StrechBar';
-export {PropsList, Prop} from './components/PropsList';
-export {Icon} from './components/Icon';
-export {Divider}  from './components/Divider';
-export {HeadingText} from './components/HeadingText';
-export {DropdownFirst} from './components/DropdownFirst';
-export {DropdownItem} from './components/DropdownFirst';
-export {DropdownLabel} from './components/DropdownFirst';
-export {DropdownDivider} from './components/DropdownFirst';
-export {Dropdown} from './components/Dropdown';
-export {Tag} from './components/Tag';
-export {TabLabel, TabPanel, TabContent, Tabs} from './components/TabCustom';
 export {Autocomplete} from './components/Autocomplete';
->>>>>>> master
 
 // declare non-typescript exports to prevent errors
 export declare const ToggleBoxNext: any;
