5fcae37c636dc1c15b2104e56db919c249f2ba78
Vladimir Stefanovic <vladimir.stefanovic@sourcefabric.org>
2021-01-15 17:54:21 +0100

    New styles for IconLabel and Label components

--- app-typescript/components/IconLabel.tsx
+++ app-typescript/components/IconLabel.tsx
@@ -5,13 +5,14 @@ interface IProps {
     text: string;
     type?: 'default' | 'primary' | 'success' | 'warning' | 'alert' | 'highlight' | 'sd-green';
     icon?: string;
+    style?: 'basic' | 'translucent'; // defaults to 'basic'
 }
 
 export class IconLabel extends React.PureComponent<IProps> {
     render() {
         let classes = classNames('label-icon', {
             [`label-icon--${this.props.type}`]: this.props.type !== undefined,
-
+            'label-icon--translucent': this.props.style === 'translucent'
         });
 
         return (
