a03f6f1f5fdfc3f7bb60f3a7f53270bd20ca89c5
Petr Jasek <jasekpetr@gmail.com>
2021-05-26 16:46:30 +0200

    simplify carousel api

--- app-typescript/components/Carousel.tsx
+++ app-typescript/components/Carousel.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
 import { Carousel as PrimeCarousel } from '@superdesk/primereact/carousel';
+import { Badge } from './Badge';
 
-interface IProps<T> {
+interface IImage {
+    src: string;
+    alt: string;
+}
+
+interface IProps {
     id?: string;
     className?: string;
     theme?: string;
     page?: number;
-    items: Array<T>;
+    images: Array<IImage>;
     circular?: boolean;
     autoplayInterval?: number;
     numVisible?: number;
     numScroll?: number;
     responsiveOptions?: Array<IPropsResponsive>;
-    headerTemplate?: JSX.Element;
-    footerTemplate?: JSX.Element;
-    itemTemplate(item: any): JSX.Element;
     onPageChange?(e: {page: number}): void;
+    title?: string;
+    imageCount?: number;
+    description?: string;
+    headerActions?: JSX.Element;
 }
 
 interface IPropsResponsive {
@@ -24,20 +31,51 @@ interface IPropsResponsive {
     numScroll: number;
 }
 
-export class Carousel<T> extends React.PureComponent<IProps<T>, {}> {
+export class Carousel extends React.PureComponent<IProps, {}> {
     render() {
+
+        const header = (
+            <div className="sd-thumb-carousel__header">
+                {this.props.title && (
+                    <h4 className="sd-thumb-carousel__heading">{this.props.title}</h4>
+                )}
+                {this.props.imageCount && (
+                    <Badge text={'' + this.props.imageCount} type='light' />
+                )}
+                {this.props.headerActions && (
+                    <div className="sd-thumb-carousel__header-block--r">
+                        {this.props.headerActions}
+                    </div>
+                )}
+            </div>
+        );
+
+        const footer = this.props.description ? (
+            <div className="sd-thumb-carousel__description">
+                {this.props.description}
+            </div>
+        ) : null;
+
+        const itemTemplate = (props: IImage) => (
+            <div className="sd-thumb-carousel__item">
+                <div className="sd-thumb-carousel__item-inner">
+                    <img src={props.src} alt={props.alt} />
+                </div>
+            </div>
+        );
+
         return <div style={{display: 'content'}}
             data-theme={this.props.theme !== 'dark' ? '' : 'dark-ui' }
             className={this.props.className}>
             <PrimeCarousel
                 id={this.props.id}
-                value={this.props.items}
+                value={this.props.images}
                 numVisible={this.props.numVisible}
                 numScroll={this.props.numScroll}
                 responsiveOptions={this.props.responsiveOptions}
-                itemTemplate={this.props.itemTemplate}
-                header={this.props.headerTemplate}
-                footer={this.props.footerTemplate}
+                itemTemplate={itemTemplate}
+                header={header}
+                footer={footer}
                 indicatorsContentClassName='sd-thumb-carousel__indicators'
                 onPageChange={this.props.onPageChange}
             />
