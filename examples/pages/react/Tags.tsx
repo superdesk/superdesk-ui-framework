692d8fe9da7cb7308f5d8bc7bcd2b31fef811372
Nemanja Pavlovic <me@pavlovicnemanja.com>
2021-11-10 13:21:04 +0100

    hotfix/2.4.2 Fixed issue with inputs

--- examples/pages/react/Tags.tsx
+++ examples/pages/react/Tags.tsx
@@ -10,7 +10,13 @@ export default class TagDoc extends React.Component<{}, ITag> {
     constructor(props){
         super(props);
         this.state = {
-            tags: [{text: 'This is a tag'},{text: 'This is another tag', shade:'darker'},{text: 'Lorem ipsum', shade:'highlight1'}, {text: 'Dolor amet', shade:'highlight2', shape:'square'}],
+            tags: [
+                {text: 'This is a tag'},
+                {text: 'This is another tag', shade:'darker'},
+                {text: 'Lorem ipsum', shade:'highlight1'},
+                {text: 'Dolor amet', shade:'highlight2', shape:'square'},
+                {text: 'Read only tag', readOnly: true}
+            ],
         }
         this.handleClick=this.handleClick.bind(this);
     }
@@ -37,7 +43,12 @@ export default class TagDoc extends React.Component<{}, ITag> {
                     {this.state.tags.map((tag,index)=>{
                             return (
                                 <React.Fragment key={index}>
-                                    <Tag keyValue={index} text={tag.text} shade={tag.shade} shape={tag.shape} onClick={()=>this.handleClick(index)}/>
+                                    <Tag keyValue={index}
+                                        text={tag.text}
+                                        shade={tag.shade}
+                                        shape={tag.shape}
+                                        readOnly={tag.readOnly}
+                                        onClick={()=>this.handleClick(index)}/>
                                 </React.Fragment>
                         )
                         })}
@@ -55,10 +66,10 @@ export default class TagDoc extends React.Component<{}, ITag> {
 
             <h3 className="docs-page__h3">Props</h3>
                 <PropsList>
-                    <Prop name='text' isRequered={true} type='string' default='/' description='Tag text value.'/>
-                    <Prop name='shade' isRequered={false} type='light | darker | highlight1 | highlight2' default='light' description='Shade colour of tag'/>
-                    <Prop name='shape' isRequered={false} type='round | square' default='round' description='Make shape of tag square or default round.'/>
-                    <Prop name='keyValue' isRequered={false} type='number' default='/' description='Value of tag key'/>
+                    <Prop name='text' isRequired={true} type='string' default='/' description='Tag text value.'/>
+                    <Prop name='shade' isRequired={false} type='light | darker | highlight1 | highlight2' default='light' description='Shade colour of tag'/>
+                    <Prop name='shape' isRequired={false} type='round | square' default='round' description='Make shape of tag square or default round.'/>
+                    <Prop name='keyValue' isRequired={false} type='number' default='/' description='Value of tag key'/>
                 </PropsList>
         </section>
         )
