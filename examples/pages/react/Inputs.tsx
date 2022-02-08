6f72ad455905a31fac093b5dcf9113f71f786e4c
FritzSF <vladimir.stefanovic@sourcefabric.org>
2022-02-08 16:01:55 +0100

    Select, Input and Tag fix

--- examples/pages/react/Inputs.tsx
+++ examples/pages/react/Inputs.tsx
@@ -33,7 +33,7 @@ export default class InputsDoc extends React.Component<{}, IState> {
                 <p className='docs-page__paragraph'></p>
                 <Markup.ReactMarkup>
                     <Markup.ReactMarkupPreview>
-                        <div className='docs-page__content-row docs-page__content-row--no-margin'>
+                        <div className='docs-page__content-row'>
                             <div className='form__row'>
                                 <CheckGroup>
                                     <Checkbox checked={this.state.required} label={{text:'Required input'}} onChange={(value) => {this.setState({required: value})}} />
@@ -45,7 +45,7 @@ export default class InputsDoc extends React.Component<{}, IState> {
 
                             <div className='form__row'>
                                 <Input label='Input label'
-                                    value='This is some value'
+                                    value='This is the value'
                                     maxLength={30}
                                     error='This is error message'
                                     info='This is some hint message'
@@ -53,6 +53,23 @@ export default class InputsDoc extends React.Component<{}, IState> {
                                     required={this.state.required}
                                     disabled={this.state.disabled}
                                     invalid={this.state.invalid}
+                                    onChange={(value) => {}} />
+                            </div>
+                        </div>
+
+                        <div className='docs-page__content-row docs-page__content-row--no-margin'>
+                            <p className="docs-page__paragraph">// Hidden label</p>
+                            <div className='form__row'>
+                                <Input label='Hidden input label'
+                                    value='Lorem ipsum input'
+                                    maxLength={30}
+                                    error='This is an error message'
+                                    info='This is a hint message'
+                                    inlineLabel={true}
+                                    labelHidden={true}
+                                    required={false}
+                                    disabled={false}
+                                    invalid={false}
                                     onChange={(value) => {}} />
                             </div>
                         </div>
@@ -80,6 +97,7 @@ export default class InputsDoc extends React.Component<{}, IState> {
                     <Prop name='info' isRequired={false} type='string' default='/' description='Hint text'/>
                     <Prop name='error' isRequired={false} type='string' default='/' description='Error text'/>
                     <Prop name='inlineLabel' isRequired={false} type='boolean' default='false' description='Position labels as inline'/>
+                    <Prop name='labelHidden' isRequired={false} type='boolean' default='false' description='Hide the label. The label will still be accessible to screen-readers.'/>
                     <Prop name='required' isRequired={false} type='boolean' default='false' description='Mark field as required'/>
                     <Prop name='disabled' isRequired={false} type='boolean' default='false' description='Mark field as disabled'/>
                     <Prop name='invalid' isRequired={false} type='boolean' default='false' description='Mark field as invalid'/>
--- examples/pages/react/Selects.tsx
+++ examples/pages/react/Selects.tsx
@@ -37,7 +37,7 @@ export default class InputsDoc extends React.Component<{}, IState> {
                 <p className='docs-page__paragraph'></p>
                 <Markup.ReactMarkup>
                     <Markup.ReactMarkupPreview>
-                        <div className='docs-page__content-row docs-page__content-row--no-margin'>
+                        <div className='docs-page__content-row'>
                             <div className='form__row'>
                                 <CheckGroup>
                                     <Checkbox checked={this.state.required} label={{ text: 'Required input' }} onChange={(value) => { this.setState({ required: value }) }} />
@@ -59,6 +59,27 @@ export default class InputsDoc extends React.Component<{}, IState> {
                                     onChange={(value) => { console.log(value) }}>
                                     <Option>Option 1</Option>
                                     <Option>Option 2</Option>
+                                </Select>
+                            </div>
+                        </div>
+
+                        <div className='docs-page__content-row docs-page__content-row--no-margin'>
+                            <p className="docs-page__paragraph">// Hidden label</p>
+                            <div className='form__row'>
+                                <Select label='Hidden select label'
+                                    value='Option 1'
+                                    error='Error message'
+                                    info='This is a hint message'
+                                    inlineLabel={true}
+                                    labelHidden={true}
+                                    required={false}
+                                    disabled={false}
+                                    invalid={this.state.invalid}
+                                    onChange={(value) => { console.log(value) }}>
+                                    <Option>Option 1</Option>
+                                    <Option>Option 2</Option>
+                                    <Option>Option 3</Option>
+                                    <Option>Option 4</Option>
                                 </Select>
                             </div>
                         </div>
@@ -87,6 +108,7 @@ export default class InputsDoc extends React.Component<{}, IState> {
                     <Prop name='info' isRequired={false} type='string' default='/' description='Hint text' />
                     <Prop name='error' isRequired={false} type='string' default='/' description='Error text' />
                     <Prop name='inlineLabel' isRequired={false} type='boolean' default='false' description='Position labels as inline' />
+                    <Prop name='labelHidden' isRequired={false} type='boolean' default='false' description='Hide the label. The label will still be accessible to screen-readers.'/>
                     <Prop name='required' isRequired={false} type='boolean' default='false' description='Mark field as required' />
                     <Prop name='disabled' isRequired={false} type='boolean' default='false' description='Mark field as disabled' />
                     <Prop name='invalid' isRequired={false} type='boolean' default='false' description='Mark field as invalid' />
