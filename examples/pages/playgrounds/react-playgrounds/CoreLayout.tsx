cadc863cb0b3917ff508d488cb996d111704efef
dzonidoo <nikola71092@gmial.com>
2022-07-14 10:31:50 +0200

    feat(BottomNav): Improve functionality for remove item.

--- examples/pages/playgrounds/react-playgrounds/CoreLayout.tsx
+++ examples/pages/playgrounds/react-playgrounds/CoreLayout.tsx
@@ -5,6 +5,7 @@ import * as Layout from '../../../../app-typescript/components/Layouts';
 import * as Form from '../../../../app-typescript/components/Form';
 import * as Nav from '../../../../app-typescript/components/Navigation';
 import { BottomNav } from '../../../../app-typescript/components/Navigation/BottomNav';
+import { clone } from 'lodash';
 
 interface IProps {
     children?: React.ReactNode;
@@ -19,6 +20,7 @@ interface IState {
     theme: 'dark' | 'light' | string;
     mainMenuOpen: boolean;
     notificationsOpen: boolean;
+    botNavArray: any;
 }
 
 export class CoreLayout extends React.Component<IProps, IState> {
@@ -28,9 +30,16 @@ export class CoreLayout extends React.Component<IProps, IState> {
             theme: 'light',
             mainMenuOpen: false,
             notificationsOpen: false,
+            botNavArray: [
+                            { icon:'photo', title: 'Sed posuere consectetur est at lobortis.', onClick: () => false, onRemove: (e) => this.handleDelete(e) },
+                            { title: 'Nullam id dolor id nibh ultricies.', onClick: () => false, onRemove: (e) => this.handleDelete(e)},
+                            { icon:'video', title: 'Nulla vitae elit libero, a pharetra augue.', onClick: () => false, onRemove: (e) => this.handleDelete(e)},
+                            { title: 'Donec sed odio dui.', onClick: () => false, onRemove: (e) => this.handleDelete(e)},
+            ]
         }
         this.handleMainMenu = this.handleMainMenu.bind(this);
         this.handleNotifications = this.handleNotifications.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
     }
 
     handleMainMenu() {
@@ -38,56 +47,64 @@ export class CoreLayout extends React.Component<IProps, IState> {
             mainMenuOpen: !state.mainMenuOpen,
         }));
     }
+
     handleNotifications() {
         this.setState((state) => ({
             notificationsOpen: !state.notificationsOpen,
         }));
     }
+    
+    handleDelete(indexNumber: number) {
+        const newItems = clone(this.state.botNavArray);
+        newItems.splice(indexNumber, 1);
 
+        this.setState({
+            botNavArray: newItems,
+        });  
+    }
 
     render() {
         return (
             <Layout.CoreLayout
-                heading='Core Layout'
-                menuOpen={this.state.mainMenuOpen}
-                onClick={this.handleMainMenu}
-                active={this.state.mainMenuOpen}
-                ariaControls='main-menu'
-                menuId='main-menu'
-                slideInMenu={(
-                    <Layout.MainMenu
-                        headerTitle='Main Menu'
-                        poweredBy='Powered by Superdesk technology'
-                        header={(null)}
-                        footerContent={true}
-                        footer={(
-                            <p>Menu footer testing</p>
-                        )}
-                    >
-                        <LeftMenu 
-                            style='blanc'
-                            reverseItemBorder={true}
-                            size='large'
-                            ariaLabel={'Left navigation'}   
-                            activeItemId='1'
-                            groups={[
-                                { label: 'MAIN SECTIONS', items: [
-                                    { id: '1', label: 'Section 1', ref:'section1'},
-                                    { id: '2', label: 'Section 2', ref: 'section2' },
-                                    { id: '3', label: 'Section 3', ref: 'section3' },
-                                
-                                ]},
-                                { label:'OTHER SECTIONS', items: [
-                                    { id: '4', label: 'Section 4', ref: 'section4' },
-                                    { id: '5', label: 'Section 5', ref: 'section5' },
-                                ]},
-                                { label:'OTHER SECTIONS', items: [
-                                    { id: '6', label: 'Section 6', ref: 'section6' },
-                                    { id: '7', label: 'Section 7', ref: 'section7' },
-                                ]}
-                            ]}
-                        onSelect={() => false} />
-                    </Layout.MainMenu>
+            heading='Core Layout'
+            menuOpen={this.state.mainMenuOpen}
+            onClick={this.handleMainMenu}
+            active={this.state.mainMenuOpen}
+            ariaControls='main-menu'
+            menuId='main-menu'
+            slideInMenu={(
+                <Layout.MainMenu
+                headerTitle='Main Menu'
+                poweredBy='Powered by Superdesk technology'
+                header={(null)}
+                footerContent={true}
+                footer={(
+                    <p>Menu footer testing</p>
+                )}>
+                    <LeftMenu 
+                    style='blanc'
+                    reverseItemBorder={true}
+                    size='large'
+                    ariaLabel={'Left navigation'}   
+                    activeItemId='1'
+                    groups={[
+                        { label: 'MAIN SECTIONS', items: [
+                            { id: '1', label: 'Section 1', ref:'section1'},
+                            { id: '2', label: 'Section 2', ref: 'section2' },
+                            { id: '3', label: 'Section 3', ref: 'section3' },
+                        
+                        ]},
+                        { label:'OTHER SECTIONS', items: [
+                            { id: '4', label: 'Section 4', ref: 'section4' },
+                            { id: '5', label: 'Section 5', ref: 'section5' },
+                        ]},
+                        { label:'OTHER SECTIONS', items: [
+                            { id: '6', label: 'Section 6', ref: 'section6' },
+                            { id: '7', label: 'Section 7', ref: 'section7' },
+                        ]}
+                    ]}
+                    onSelect={() => false} />
+                </Layout.MainMenu>
                 )}
                 topMenu={(
                     <NavButton badgeValue='6' icon='bell' text='Show notifications' onClick={this.handleNotifications} />
@@ -96,22 +113,16 @@ export class CoreLayout extends React.Component<IProps, IState> {
                     <>
                     <Layout.BottomBarAction onClick={()=> false} />
                     <BottomNav
-                        items={[
-                            { icon:'photo', title: 'Sed posuere consectetur est at lobortis.', onClick: () => console.log('test1') },
-                            { title: 'Nullam id dolor id nibh ultricies.', onClick: () => console.log('test2')},
-                            { icon:'video', title: 'Nulla vitae elit libero, a pharetra augue.', onClick: () => false},
-                            { title: 'Donec sed odio dui.', onClick: () => false},
-                    ]} />
+                        items={this.state.botNavArray} />
                     </>
                 )}
                 overlay={(
                     <Layout.NotificationPanel
-                        header={(null)}
-                        headerTitle='Notifications'
-                        open={this.state.notificationsOpen}
-                        onClick={this.handleNotifications}
-                        theme='dark'
-                    >
+                    header={(null)}
+                    headerTitle='Notifications'
+                    open={this.state.notificationsOpen}
+                    onClick={this.handleNotifications}
+                    theme='dark'>
                         <BoxedList>
                             <BoxedListItem unread={true}>
                                 <Time datetime='29.06.2022'>29.06.2022</Time>
@@ -137,8 +148,7 @@ export class CoreLayout extends React.Component<IProps, IState> {
                         </BoxedList>
 
                     </Layout.NotificationPanel>
-                )}
-            >
+                )}>
             </Layout.CoreLayout>
         );
     }
