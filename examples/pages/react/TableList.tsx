9d1639c2daa7e8698cd1e7d80431f706a33e9611
dzonidoo <nikola71092@gmial.com>
2022-10-17 13:01:46 +0200

    fix(TableList): fixed functionality for adding item.

--- examples/pages/react/TableList.tsx
+++ examples/pages/react/TableList.tsx
@@ -5,6 +5,7 @@ import { TableList, TableListItem } from '../../../app-typescript/components/Lis
 
 interface IState {
     array: any;
+    itemsDropdown: any;
 }
 
 interface IProps {
@@ -14,7 +15,13 @@ interface IProps {
 export default class TableListDoc extends React.Component<IProps, IState> {
     constructor(props: IState) {
         super(props);
-        this.state = {
+        this.state = {                             
+            itemsDropdown: [
+                { label: <Label style='translucent' type='primary' text='aacc' />, onSelect: () => console.log('a')
+                },
+                { label: <Label style='translucent' text='prlg' />, onSelect: () => 1 },
+                { label: <Label style='translucent' type='primary' text='prlg' />, onSelect: () => 1 },
+            ],
             array: [
                 {
                     start: <>
@@ -71,10 +78,10 @@ export default class TableListDoc extends React.Component<IProps, IState> {
                     dragAndDrop
                     addItem
                     array={this.state.array}
-                    itemsDropdown={[
-                        { label: 'Action 1', onSelect: () => 1 },
-                        { label: 'Action 2', onSelect: () => 1 },
-                        { label: 'Action 3', onSelect: () => 1 },
+                    itemsDropdown={(index) => [
+                        { label: <Label style='translucent' type='primary' text='aacc' />, onSelect: () => console.log(index) },
+                        { label: <Label style='translucent' text='prlg' />, onSelect: () => console.log(index) },
+                        { label: <Label style='translucent' type='primary' text='prlg' />, onSelect: () => console.log(index) },
                     ]} />
                 `}
                 </Markup.ReactMarkupCodePreview>
@@ -82,32 +89,31 @@ export default class TableListDoc extends React.Component<IProps, IState> {
                 <p className="docs-page__paragraph">With drag and drop functionality:</p>
                 <Markup.ReactMarkup>
                     <Markup.ReactMarkupPreview>
-
                         <TableList
                             dragAndDrop
                             addItem
-                            onAddItem={(index, item) => console.log(index, item)}
                             array={this.state.array}
-                            itemsDropdown={[
-                                { label: <Label style='translucent' type='primary' text='aacc' />, onSelect: () => 1},
-                                { label: <Label style='translucent' text='prlg' />, onSelect: () => 1 },
-                                { label: <Label style='translucent' type='primary' text='prlg' />, onSelect: () => 1 },
-                            ]}
+                            itemsDropdown={(index) => [
+                                    { label: <Label style='translucent' type='primary' text='aacc' />, onSelect: () => console.log(index) },
+                                    { label: <Label style='translucent' text='prlg' />, onSelect: () => console.log(index) },
+                                    { label: <Label style='translucent' type='primary' text='prlg' />, onSelect: () => console.log(index) },
+                                ]
+                            }
                             onDrag={(start, end) => console.log(start, end)}
                         />
-
                     </Markup.ReactMarkupPreview>
                     <Markup.ReactMarkupCode>{`
                         <TableList
                         dragAndDrop
                         addItem
                         array={this.state.array}
-                        itemsDropdown={[
-                            { label: <Label style='translucent' type='primary' text='aacc' />, onSelect: () => 1 },
-                            { label: <Label style='translucent' text='prlg' />, onSelect: () => 1 },
-                            { label: <Label style='translucent' type='primary' text='prlg' />, onSelect: () => 1 },
-                        ]}
-                        onClick={() => false}
+                        itemsDropdown={(index) => [
+                                { label: <Label style='translucent' type='primary' text='aacc' />, onSelect: () => console.log(index) },
+                                { label: <Label style='translucent' text='prlg' />, onSelect: () => console.log(index) },
+                                { label: <Label style='translucent' type='primary' text='prlg' />, onSelect: () => console.log(index) },
+                            ]
+                        }
+                        onDrag={(start, end) => console.log(start, end)}
                         />
                     `}
                     </Markup.ReactMarkupCode>
@@ -116,16 +122,15 @@ export default class TableListDoc extends React.Component<IProps, IState> {
                 <h3 className="docs-page__h3">Props</h3>
                 <p className="docs-page__paragraph">TableList</p>
                 <PropsList>
-                    <Prop name='array' isRequired={false} type='Array' default='false' description='Array of object.' />
-                    <Prop name='children' isRequired={false} type='React.ReactNode' default='false' description='Children of component.' />
+                    <Prop name='array' isRequired={true} type='Array' default='false' description='Array of object.' />
                     <Prop name='addItem' isRequired={false} type='boolean' default='false' description='Functionality to add items to the list.' />
                     <Prop name='dragAndDrop' isRequired={false} type='boolean' default='false' description='Drag&Drop functionality.' />
-                    <Prop name='itemsDropdown' isRequired={false} type='React.ReactNode | any' default='false' description='Dropdown for functionality to add items to the list.' />
                     <Prop name='className' isRequired={false} type='string' default='false' description='Add class on TableList container.' />
-                    <Prop name='showDragHandle' isRequired={false} type='string' default='always' description='' />
-                    <Prop name='onClick' isRequired={false} type='function' default='false' description='onClick functionality.' />
+                    <Prop name='showDragHandle' isRequired={false} type='string' default='always' description='"always" | "onHover" | "none".' />
+                    <Prop name='readOnly' isRequired={false} type='boolean' default='false' description='When specified, component changes are not enabled.' />
                     <Prop name='onDrag' isRequired={false} type='function' default='false' description='Returns start and end position of draggable item' />
                     <Prop name='onAddItem' isRequired={false} type='function' default='false' description='Returns index of draggable item.' />
+                    <Prop name='itemsDropdown' isRequired={false} type='function' default='false' description='Dropdown for adding items in the list. Returns index of draggable item.' />
                 </PropsList>
                 <p className="docs-page__paragraph">array:</p>
                 <PropsList>
@@ -133,6 +138,7 @@ export default class TableListDoc extends React.Component<IProps, IState> {
                     <Prop name='center' isRequired={false} type='React.ReactNode' default='false' description='Column of individual items of list.' />
                     <Prop name='end' isRequired={false} type='React.ReactNode' default='false' description='Column of individual items of list.' />
                     <Prop name='action' isRequired={false} type='React.ReactNode' default='false' description='Column of individual list items that is displayed on hover.' />
+                    <Prop name='hexColor' isRequired={false} type='string' default='false' description='' />
                     <Prop name='onClick' isRequired={false} type='function' default='false' description='onClick functionality.' />
                     <Prop name='onDoubleClick' isRequired={false} type='function' default='false' description='onDoubleClick functionality.' />
                 </PropsList>
