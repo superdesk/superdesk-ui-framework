d2f9c9b3a14eab2e7bf6d8a48a87aafd0dc1a4d1
dzonidoo <nikola71092@gmial.com>
2022-12-21 11:18:56 +0100

    feat(CoreLeyout): Move all functionality on CoreLayout page and improve functionality for full width editor

--- app-typescript/components/Navigation/SideBarMenu.tsx
+++ app-typescript/components/Navigation/SideBarMenu.tsx
@@ -4,6 +4,9 @@ import { Icon } from '../Icon';
 interface IProps {
     items: Array<IItem | 'divider'>;
     side?: 'none' | 'left' | 'right';
+    hover?: boolean;
+    editor?: boolean;
+    onCLick?(): void;
 }
 
 interface IItem {
@@ -11,11 +14,15 @@ interface IItem {
     size: 'small' | 'big'; // defaults to 'small'
     tooltip?: string;
     active?: boolean;
+    hover?: boolean;
+    editor?: boolean;
+    onCLick?(): void;
 }
 
 interface IState {
     index: number;
     closeIndex: number;
+    editor: boolean;
 }
 
 export class SideBarMenu extends React.PureComponent<IProps, IState> {
@@ -24,6 +31,7 @@ export class SideBarMenu extends React.PureComponent<IProps, IState> {
         this.state = {
             index: -1,
             closeIndex: -1,
+            editor: this.props.editor ? this.props.editor : false,
         };
         this.handleClick = this.handleClick.bind(this);
     }
@@ -31,6 +39,7 @@ export class SideBarMenu extends React.PureComponent<IProps, IState> {
     handleClick(indexNumber: number) {
         this.setState({
             index: indexNumber,
+            editor: !this.state.editor,
         });
         if (this.state.index === indexNumber) {
             this.setState({
@@ -42,7 +51,7 @@ export class SideBarMenu extends React.PureComponent<IProps, IState> {
     render() {
         return (
             <div className='sd-sidebar-menu sd-content-wrapper__left-tabs'>
-                <ul className='authoring-active'>
+                <ul className={'authoring-active'}>
                     {this.props.items.map((item, index) => {
                         if (item === 'divider') {
                             return (
@@ -50,9 +59,21 @@ export class SideBarMenu extends React.PureComponent<IProps, IState> {
                             );
                         } else {
                             return (
-                                <li key={index} data-sd-tooltip={item['tooltip']} data-flow='right'>
-                                    <a className={'sd-sidebar-menu__btn' + (index === this.state.closeIndex ? ' sd-sidebar-menu__btn--closed ' : '') + (item['active'] ? ' sd-sidebar-menu__btn--active' : (index === this.state.index ? ' sd-sidebar-menu__btn--active' : ''))}
-                                        onClick={() => this.handleClick(index)}>
+                                <li key={index}
+                                data-sd-tooltip={item['tooltip']}
+                                data-flow='right'
+                                className={item.editor ? 'authoring-active__item' : ''}>
+                                    <a className={'sd-sidebar-menu__btn'
+                                    + (this.state.editor ? ' sd-sidebar-menu__btn--closed ' : '')
+                                    + (item['active'] ? ' sd-sidebar-menu__btn--active' : (index === this.state.index ? ' sd-sidebar-menu__btn--active' : ''))}
+                                        onClick={() => {
+                                            this.handleClick(index);
+                                            if (item.editor) {
+                                                if (item.onCLick) {
+                                                    item.onCLick();
+                                                }
+                                            }
+                                        }}>
                                         <span className='sd-sidebar-menu__main-icon '>
                                             <Icon size={item['size']} name={item['icon']} />
                                         </span>
