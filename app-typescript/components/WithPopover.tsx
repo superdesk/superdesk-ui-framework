dc614504ffb92a10b68bcccf67ade67f95d9fbe6
Tomas Kikutis <t.kikutis@gmail.com>
2023-05-08 13:05:48 +0200

    add with popup higher order component from client-core

---
+++ app-typescript/components/WithPopover.tsx
@@ -0,0 +1,43 @@
+
+import * as React from 'react';
+import {Placement} from '@popperjs/core';
+import {showPopup} from './ShowPopup';
+
+export interface IPropsWithPopover {
+    children(toggle: (referenceElement: HTMLElement) => void): React.ReactNode;
+    placement: Placement;
+    component: React.ComponentType<{closePopup(): void}>;
+    zIndex?: number;
+    closeOnHoverEnd?: boolean;
+    onClose?: () => void;
+}
+
+export class WithPopover extends React.PureComponent<IPropsWithPopover> {
+    private closePopup?: () => void;
+
+    constructor(props: IPropsWithPopover) {
+        super(props);
+
+        this.togglePopup = this.togglePopup.bind(this);
+    }
+
+    togglePopup(referenceElement: HTMLElement) {
+        if (this.closePopup != null) {
+            this.closePopup();
+            this.closePopup = undefined;
+        } else {
+            this.closePopup = showPopup(
+                referenceElement,
+                this.props.placement,
+                this.props.component,
+                this.props.zIndex,
+                this.props.closeOnHoverEnd,
+                this.props.onClose,
+            ).close;
+        }
+    }
+
+    render(): React.ReactNode {
+        return this.props.children(this.togglePopup);
+    }
+}
