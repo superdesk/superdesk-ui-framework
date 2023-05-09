f455b86c89b6cbe8281f29faaf24033066d9486c
Nikola Stojanovic <68916411+dzonidoo@users.noreply.github.com>
2023-05-09 16:25:38 +0200

    Panel pinned (#757)

--- app-typescript/components/IconButton.tsx
+++ app-typescript/components/IconButton.tsx
@@ -25,20 +25,26 @@ export class IconButton extends React.PureComponent<IProps> {
             [`icn-btn--${this.props.style}`]: this.props.style || this.props.style !== undefined,
             'icn-btn--disabled': this.props.disabled,
         });
+
         return (
             <Tooltip
-            text={this.props.ariaValue}
-            flow={this.props.toolTipFlow}
-            appendToBody={this.props.toolTipAppend}
-            disabled={this.props.disabled}>
+                text={this.props.ariaValue}
+                flow={this.props.toolTipFlow}
+                appendToBody={this.props.toolTipAppend}
+                disabled={this.props.disabled}
+            >
                 <button
                     id={this.props.id}
                     tabIndex={0}
                     onClick={this.props.onClick}
                     className={classes}
                     disabled={this.props.disabled}
-                    aria-label={this.props.ariaValue}>
-                    <Icon name={this.props.icon} ariaHidden={true}/>
+                    aria-label={this.props.ariaValue}
+                >
+                    <Icon
+                        name={this.props.icon}
+                        ariaHidden={true}
+                    />
                 </button>
             </Tooltip>
         );
