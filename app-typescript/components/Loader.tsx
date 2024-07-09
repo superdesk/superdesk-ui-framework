bd61eb3e80ad03d7908ae68fbdf363b6ececa94a
Tomas Kikutis <t.kikutis@gmail.com>
2024-07-09 17:40:07 +0200

    add inline loader

--- app-typescript/components/Loader.tsx
+++ app-typescript/components/Loader.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
 
-interface IProps {
-    overlay?: boolean;
+interface IPropsOverlay {
+    overlay: true;
 }
+
+interface IPropsInline {
+    overlay?: false;
+    width?: React.CSSProperties['width'];
+    height?: React.CSSProperties['height'];
+    backgroundColor?: React.CSSProperties['backgroundColor'];
+}
+
+type IProps = IPropsOverlay | IPropsInline;
 
 export class Loader extends React.Component<IProps> {
     render() {
-
         if (this.props.overlay) {
             return (
-                <div className='sd-loader'></div>
+                <div className="sd-loader" />
             );
         } else {
-            return null;
+            return (
+                <div
+                    className="sd-loader--inline"
+                    style={{
+                        width: this.props.width,
+                        height: this.props.height,
+                        backgroundColor: this.props.backgroundColor,
+                    }}
+                />
+            );
         }
     }
 }
