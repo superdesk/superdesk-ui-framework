101c3ba03c7b9bb7d64138e9b378017672a07bf8
dzonidoo <nikola71092@gmail.com>
2025-03-20 16:42:03 +0100

    fix lint

--- app-typescript/components/TreeMenu.tsx
+++ app-typescript/components/TreeMenu.tsx
@@ -208,11 +208,11 @@ export class TreeMenu<T> extends React.Component<IProps<T>, IState<T>> {
                     onFirstUpdate: (state) => {
                         this.popperInstance?.setOptions({
                             placement: state.placement,
-                            modifiers: this.popperInstance.state.options.modifiers.map(modifier =>
+                            modifiers: this.popperInstance.state.options.modifiers.map((modifier) => (
                                 modifier.name === 'flip'
                                     ? {...modifier, enabled: false}
                                     : modifier
-                            ),
+                            )),
                         });
                     },
                 });
