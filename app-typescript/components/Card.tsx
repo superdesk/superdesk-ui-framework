c32ae1fbf0bd6285b22db183b208570533bd389b
Tomas Kikutis <t.kikutis@gmail.com>
2025-06-11 14:10:23 +0200

    card improvements - fix prop overwrite paddingBlock was not taking effect due to paddingBlockStart evaluating to undefined

--- app-typescript/components/Card.tsx
+++ app-typescript/components/Card.tsx
@@ -9,26 +9,49 @@ interface IProps extends Pick<React.CSSProperties, 'paddingBlock'> {
     paddingInline?: React.CSSProperties['paddingInline'];
     paddingInlineStart?: React.CSSProperties['paddingInlineStart'];
     paddingInlineEnd?: React.CSSProperties['paddingInlineEnd'];
+    style?: React.CSSProperties;
+    'data-test-id'?: string;
 }
 
 export class Card extends React.PureComponent<IProps> {
     render() {
+        const style: React.CSSProperties = {
+            width: '100%',
+            background: 'var(--sd-item__main-Bg)',
+            borderRadius: 'var(--b-radius--medium)',
+            boxShadow: 'var(--sd-shadow--z2)',
+        };
+
+        if (this.props.paddingBase != null) {
+            style.padding = `calc( ${this.props.paddingBase} * var(--base-increment))`;
+        }
+
+        if (this.props.paddingBlock != null) {
+            style.paddingBlock = this.props.paddingBlock;
+        }
+
+        if (this.props.paddingBlockStart != null) {
+            style.paddingBlockStart = this.props.paddingBlockStart;
+        }
+
+        if (this.props.paddingBlockEnd != null) {
+            style.paddingBlockEnd = this.props.paddingBlockEnd;
+        }
+
+        if (this.props.paddingInline != null) {
+            style.paddingInline = this.props.paddingInline;
+        }
+
+        if (this.props.paddingInlineStart != null) {
+            style.paddingInlineStart = this.props.paddingInlineStart;
+        }
+
+        if (this.props.paddingInlineEnd != null) {
+            style.paddingInlineEnd = this.props.paddingInlineEnd;
+        }
+
         return (
-            <div
-                style={{
-                    width: '100%',
-                    background: 'var(--sd-item__main-Bg)',
-                    borderRadius: 'var(--b-radius--medium)',
-                    padding: `calc( ${this.props.paddingBase} * var(--base-increment))`,
-                    boxShadow: 'var(--sd-shadow--z2)',
-                    paddingBlock: this.props.paddingBlock,
-                    paddingBlockStart: this.props.paddingBlockStart,
-                    paddingBlockEnd: this.props.paddingBlockEnd,
-                    paddingInline: this.props.paddingInline,
-                    paddingInlineStart: this.props.paddingInlineStart,
-                    paddingInlineEnd: this.props.paddingInlineEnd,
-                }}
-            >
+            <div style={{...style, ...this.props.style}} data-test-id={this.props['data-test-id']}>
                 {this.props.children}
             </div>
         );
