2330447b1642d8f3baea85efc944c51c793eb1a8
Tomas Kikutis <t.kikutis@gmail.com>
2025-06-27 15:59:57 +0200

    use classNames package

--- app-typescript/components/TimePicker.tsx
+++ app-typescript/components/TimePicker.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import nextId from 'react-id-generator';
+import classNames from 'classnames';
 import {InputWrapper} from './Form';
 import {IInputWrapper} from './Form/InputWrapper';
 import {TimePickerPopover} from './TimePickerPopover';
@@ -113,7 +114,9 @@ export class TimePicker extends React.PureComponent<IProps, IState> {
                                 });
                             }
                         }}
-                        className={`sd-input__input${this.props.value ? ' sd-input__input--has-value' : ''}`}
+                        className={classNames('sd-input__input', {
+                            'sd-input__input--has-value': this.props.value != null,
+                        })}
                         id={this.htmlId}
                         aria-labelledby={this.htmlId + 'label'}
                         step={this.props.allowSeconds ? 1 : undefined}
