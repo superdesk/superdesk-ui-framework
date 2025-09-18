d5423e56a8cb5118d0d2f62dcbf6f808db899d8f
Tomas Kikutis <t.kikutis@gmail.com>
2025-09-18 10:48:23 +0200

    Support jsx as tooltip content (#963)

--- app-typescript/components/Tooltip.tsx
+++ app-typescript/components/Tooltip.tsx
@@ -1,15 +1,9 @@
 import * as React from 'react';
-import nextId from 'react-id-generator';
-import tippy, {Instance, Placement} from 'tippy.js';
 import {assertNever} from '../helpers';
+import {IPropsTooltipV2, TooltipV2} from './TooltipV2';
+import {Placement} from 'popper.js';
 
-interface IProps {
-    text: string | undefined | null;
-    flow?: 'top' | 'left' | 'right' | 'down'; // defaults to 'top'
-    children(options: {attributes: {[name: string]: string}}): React.ReactNode;
-}
-
-function flowToPlacement(flow: IProps['flow']): Placement | undefined {
+function flowToPlacement(flow: IPropsLegacy['flow']): Placement | undefined {
     switch (flow) {
         case undefined:
             return undefined;
@@ -26,83 +20,29 @@ function flowToPlacement(flow: IProps['flow']): Placement | undefined {
     }
 }
 
-const tooltipAttributeName = 'data-with-tooltip';
-const getTooltipSelector = (value: string) => `[${tooltipAttributeName}=${value}]`;
-
-export class WithTooltip extends React.PureComponent<IProps> {
-    private id: string;
-    private instance: Instance | null;
-
-    constructor(props: IProps) {
-        super(props);
-
-        this.id = nextId();
-
-        this.instance = null;
-    }
-
-    private setupTooltip() {
-        const placement = flowToPlacement(this.props.flow ?? 'top');
-        const content = this.props.text;
-
-        if (this.instance == null) {
-            this.instance = tippy(getTooltipSelector(this.id), {
-                placement: placement,
-            })[0];
-
-            if (this.instance == null) {
-                // prevent crashing in unit tests
-                return;
-            }
-
-            if (content != null) {
-                this.instance.setContent(content);
-            } else {
-                this.instance.hide();
-                this.instance.disable();
-            }
-        }
-
-        const willBeEnabled = content != null;
-        const isEnabled = this.instance.state.isEnabled;
-
-        if (isEnabled && willBeEnabled) {
-            this.instance.setContent(content);
-        } else if (isEnabled) {
-            // enabled now, but needs to be disabled
-            this.instance.hide();
-            this.instance.disable();
-        } else if (willBeEnabled) {
-            // disabled now, but needs to be enabled
-            this.instance.setContent(content);
-            this.instance.enable();
-            this.instance.show();
-        }
-    }
-
-    componentDidMount(): void {
-        this.setupTooltip();
-    }
-
-    componentDidUpdate(): void {
-        this.setupTooltip();
-    }
-
-    render() {
-        return this.props.children({attributes: {[tooltipAttributeName]: this.id}});
-    }
+interface IPropsLegacy {
+    text: string | undefined | null;
+    flow?: 'top' | 'left' | 'right' | 'down'; // defaults to 'top'
+    content?: never; // added for discriminated union support with IPropsTooltipV2
 }
 
-export class Tooltip extends React.PureComponent<Omit<IProps, 'children'>> {
+type IProps = IPropsTooltipV2 | IPropsLegacy;
+
+export class Tooltip extends React.PureComponent<IProps> {
     render() {
-        return (
-            <WithTooltip text={this.props.text} flow={this.props.flow}>
-                {({attributes}) => (
-                    <div {...attributes} style={{display: 'inline-flex'}}>
-                        {this.props.children}
-                    </div>
-                )}
-            </WithTooltip>
-        );
+        if (this.props.content != null) {
+            return <TooltipV2 {...this.props} />;
+        } else {
+            // backwards compatibility for legacy props
+            return (
+                <TooltipV2 content={this.props.text ?? ''} placement={flowToPlacement(this.props.flow)}>
+                    {({attributes}) => (
+                        <div {...attributes} style={{display: 'inline-flex'}}>
+                            {this.props.children}
+                        </div>
+                    )}
+                </TooltipV2>
+            );
+        }
     }
 }
