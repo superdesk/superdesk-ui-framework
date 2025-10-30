3c17516f5c2965f967772b2eeadb560990c96535
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-10-30 11:07:42 +0200

    [SDBELGA-995] - Fix clear button handling (#975)

--- app-typescript/components/DatePicker.tsx
+++ app-typescript/components/DatePicker.tsx
@@ -304,6 +304,7 @@ export class DatePickerISO extends React.PureComponent<IDatePickerISO> {
                 }}
                 disabled={this.props.disabled}
                 preview={this.props.preview}
+                hideClearButton={this.props.hideClearButton}
                 headerButtonBar={this.props.headerButtonBar}
                 dateFormat={this.props.dateFormat}
                 locale={this.props.locale}
