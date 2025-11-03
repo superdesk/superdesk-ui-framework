016eec1ea5244e61ddc1bd2965aec8acfae4bddb
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-11-03 14:33:42 +0200

    Fix am pm switching (#978)

--- app-typescript/components/TimePicker/index.tsx
+++ app-typescript/components/TimePicker/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import nextId from 'react-id-generator';
 import classNames from 'classnames';
-import {InputWrapper} from './Form';
-import {IInputWrapper} from './Form/InputWrapper';
+import {InputWrapper} from '../Form';
+import {IInputWrapper} from '../Form/InputWrapper';
 import {TimePickerPopover} from './TimePickerPopover';
-import {PopupPositioner} from './ShowPopup';
-import {Icon} from './Icon';
-import {IconButton} from './IconButton';
+import {PopupPositioner} from '../ShowPopup';
+import {Icon} from '../Icon';
+import {IconButton} from '../IconButton';
 
 interface IProps extends IInputWrapper {
     value: string | null; // ISO8601 time string(e.g. 16:55) or null if there's no value
@@ -71,11 +71,6 @@ export class TimePicker extends React.PureComponent<IProps, IState> {
                         <TimePickerPopover
                             value={this.props.value}
                             onChange={this.props.onChange}
-                            closePopup={() => {
-                                this.setState({
-                                    popupOpen: false,
-                                });
-                            }}
                             allowSeconds={this.props.allowSeconds}
                             headerTemplate={this.props.headerTemplate}
                             footerTemplate={this.props.footerTemplate}
@@ -90,18 +85,12 @@ export class TimePicker extends React.PureComponent<IProps, IState> {
                         ref={this.timeInputRef}
                         value={this.props.value ?? ''}
                         type="time"
-                        onClick={(e) => {
-                            // don't show default popup
-                            e.preventDefault();
-
+                        onClick={() => {
                             this.setState({
                                 popupOpen: true,
                             });
                         }}
                         onKeyDown={(event) => {
-                            // don't show default popup
-                            event.preventDefault();
-
                             if (event.key === ' ') {
                                 this.setState({
                                     popupOpen: !this.state.popupOpen,
