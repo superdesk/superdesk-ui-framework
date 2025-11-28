c920ae9022bfc8fdfc6e17473678dff81f04dee7
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-11-28 15:02:30 +0200

    Implement shouldComponentUpdate

--- app-typescript/components/TreeSelect/TreeSelect.tsx
+++ app-typescript/components/TreeSelect/TreeSelect.tsx
@@ -147,6 +147,26 @@ export class TreeSelect<T> extends React.Component<IProps<T>, IState<T>> {
         this.changesFromOutside = false;
     }
 
+    shouldComponentUpdate(nextProps: Readonly<IProps<T>>, nextState: Readonly<IState<T>>): boolean {
+        if (!isEqual(this.state, nextState)) {
+            return true;
+        }
+
+        const propKeys = Object.keys(nextProps) as Array<keyof IProps<T>>;
+
+        for (const key of propKeys) {
+            if (typeof this.props[key] === 'function') {
+                continue;
+            }
+
+            if (!isEqual(this.props[key], nextProps[key])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     inputFocus = () => {
         this.inputRef.current?.focus();
     };
@@ -504,17 +524,17 @@ export class TreeSelect<T> extends React.Component<IProps<T>, IState<T>> {
             const filteredArr: Array<ITreeNode<T>> =
                 this.props.search != null
                     ? this.props
-                          .search(
-                              list.map(({value}) => value),
-                              this.state.searchFieldValue,
-                          )
-                          .map((item) => listItemsById[this.props.getId(item)])
+                        .search(
+                            list.map(({value}) => value),
+                            this.state.searchFieldValue,
+                        )
+                        .map((item) => listItemsById[this.props.getId(item)])
                     : list.filter((item) => {
-                          return this.props
-                              .getLabel(item.value)
-                              .toLowerCase()
-                              .includes(this.state.searchFieldValue.toLowerCase());
-                      });
+                        return this.props
+                            .getLabel(item.value)
+                            .toLowerCase()
+                            .includes(this.state.searchFieldValue.toLowerCase());
+                    });
 
             if (filteredArr.length === 0) {
                 return <li className="suggestion-item--nothing-found">{gettext('No results found')}</li>;
@@ -676,13 +696,13 @@ export class TreeSelect<T> extends React.Component<IProps<T>, IState<T>> {
                     kind={
                         this.props.allowMultiple
                             ? {
-                                  mode: 'multi-select',
-                                  getBackgroundColor: this.props.getBackgroundColor,
-                              }
+                                mode: 'multi-select',
+                                getBackgroundColor: this.props.getBackgroundColor,
+                            }
                             : {
-                                  mode: 'single-select',
-                                  getBorderColor: this.props.getBorderColor,
-                              }
+                                mode: 'single-select',
+                                getBorderColor: this.props.getBorderColor,
+                            }
                     }
                     items={this.state.value}
                     valueTemplate={this.props.valueTemplate}
@@ -693,35 +713,35 @@ export class TreeSelect<T> extends React.Component<IProps<T>, IState<T>> {
 
         const ListWrapper = this.props.sortable
             ? ({children}: {children: React.ReactNode}) => (
-                  <DragDropContext onDragEnd={this.onDragEnd}>
-                      <Droppable droppableId="droppable" direction="horizontal">
-                          {(provided, _snapshot) => (
-                              <ul className="tags-input__tag-list" ref={provided.innerRef} {...provided.droppableProps}>
-                                  {children}
-                                  {provided.placeholder}
-                              </ul>
-                          )}
-                      </Droppable>
-                  </DragDropContext>
-              )
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <Droppable droppableId="droppable" direction="horizontal">
+                        {(provided, _snapshot) => (
+                            <ul className="tags-input__tag-list" ref={provided.innerRef} {...provided.droppableProps}>
+                                {children}
+                                {provided.placeholder}
+                            </ul>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+            )
             : ({children}: {children: React.ReactNode}) => <ul className="tags-input__tag-list">{children}</ul>;
 
         const ItemWrapper = this.props.sortable
             ? ({children, itemId, i}: {children: React.ReactNode; itemId: string; i: number}) => {
-                  return (
-                      <Draggable draggableId={itemId} index={i}>
-                          {(provided2) => (
-                              <div
-                                  ref={provided2.innerRef}
-                                  {...provided2.draggableProps}
-                                  {...provided2.dragHandleProps}
-                              >
-                                  {children}
-                              </div>
-                          )}
-                      </Draggable>
-                  );
-              }
+                return (
+                    <Draggable draggableId={itemId} index={i}>
+                        {(provided2) => (
+                            <div
+                                ref={provided2.innerRef}
+                                {...provided2.draggableProps}
+                                {...provided2.dragHandleProps}
+                            >
+                                {children}
+                            </div>
+                        )}
+                    </Draggable>
+                );
+            }
             : ({children}: {children: React.ReactNode}) => <React.Fragment>{children}</React.Fragment>;
 
         return (
@@ -805,18 +825,18 @@ export class TreeSelect<T> extends React.Component<IProps<T>, IState<T>> {
 
                             {this.state.value.length > 0
                                 ? this.props.readOnly ||
-                                  this.props.disabled || (
-                                      <button
-                                          className="tags-input__remove-value"
-                                          style={{position: 'relative', bottom: '2px'}}
-                                          onClick={(e) => {
-                                              e.stopPropagation();
-                                              this.setState({value: []});
-                                          }}
-                                      >
-                                          <Icon name="remove-sign"></Icon>
-                                      </button>
-                                  )
+                                this.props.disabled || (
+                                    <button
+                                        className="tags-input__remove-value"
+                                        style={{position: 'relative', bottom: '2px'}}
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            this.setState({value: []});
+                                        }}
+                                    >
+                                        <Icon name="remove-sign"></Icon>
+                                    </button>
+                                )
                                 : null}
                         </div>
                     ) : (
