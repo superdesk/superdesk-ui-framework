2552e4daba55c876c41c46bd5d0776ce5ce12215
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-12-04 14:12:03 +0200

    Upgrade to node 22

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const path = require('path');
@@ -50,17 +50,17 @@ const config = {
             },
             {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader!sass-loader',
-                }),
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    },
+                    'sass-loader',
+                ],
             },
             {
-                test: /\.css/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader',
-                }),
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /\.html$/,
@@ -71,6 +71,7 @@ const config = {
                 loader: 'file-loader',
                 options: {
                     name: '[name].[ext]',
+                    esModule: false,
                 },
             },
         ],
@@ -78,15 +79,19 @@ const config = {
 
     plugins: [
         new CleanWebpackPlugin(['dist', 'react']),
+
         new HtmlWebpackPlugin({
             template: 'examples/index.html',
             chunks: ['vendor', 'examples', 'superdesk-ui'],
             chunksSortMode: 'manual',
         }),
+
         new CopyWebpackPlugin([{from: 'examples/img/', flatten: true}, {from: 'examples/pages/'}]),
-        new ExtractTextPlugin({
+
+        new MiniCssExtractPlugin({
             filename: '[name].bundle.css',
         }),
+
         new webpack.ProvidePlugin({
             $: 'jquery',
             'window.$': 'jquery',
