6bd67ed8ae582bcbf3759b57b64a285c51ab254d
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-12-08 10:10:26 +0200

    Improvements

--- app-typescript/components/CopyableTextBox.tsx
+++ app-typescript/components/CopyableTextBox.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import {Button} from './Button';
 import {gettext} from '../translations';
 
-interface IProps {
+interface ICopyableTextBoxProps {
     value: string;
     label?: string;
     helperText?: string;
@@ -15,61 +15,51 @@ interface IProps {
     'data-test-id'?: string;
 }
 
-interface IState {
-    copied: boolean;
-}
+export const CopyableTextBox: React.FC<ICopyableTextBoxProps> = (props) => {
+    const [copied, setCopied] = React.useState(false);
+    const timeoutIdRef = React.useRef<number>();
 
-export class CopyableTextBox extends React.Component<IProps, IState> {
-    private timeoutId?: number;
-
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            copied: false,
+    useEffect(() => {
+        return () => {
+            if (timeoutIdRef.current) {
+                window.clearTimeout(timeoutIdRef.current);
+            }
         };
-    }
+    }, []);
 
-    componentWillUnmount(): void {
-        if (this.timeoutId) {
-            window.clearTimeout(this.timeoutId);
-        }
-    }
+    const handleCopy = () => {
+        navigator.clipboard.writeText(props.value).then(() => {
+            setCopied(true);
 
-    handleCopy = () => {
-        navigator.clipboard.writeText(this.props.value).then(() => {
-            this.setState({copied: true});
-
-            if (this.timeoutId) {
-                window.clearTimeout(this.timeoutId);
+            if (timeoutIdRef.current) {
+                window.clearTimeout(timeoutIdRef.current);
             }
 
-            this.timeoutId = window.setTimeout(() => {
-                this.setState({copied: false});
+            timeoutIdRef.current = window.setTimeout(() => {
+                setCopied(false);
             }, 2000);
         });
     };
 
-    render() {
-        return (
-            <div className="sd-display-flex-column" data-test-id={this.props['data-test-id']}>
-                {this.props.label && <div className="sd-input__label">{this.props.label}</div>}
-                <div className="sd-d-flex gap-0-5">
-                    <div className="sd-copyable-text-box">
-                        <span className="sd-copyable-text-box__text">{this.props.value}</span>
-                    </div>
-                    <Button
-                        text={gettext('Copy')}
-                        icon={this.state.copied ? 'ok' : 'copy'}
-                        iconOnly={true}
-                        onClick={this.handleCopy}
-                        type="default"
-                        style="hollow"
-                        size={this.props.size ?? 'normal'}
-                        data-test-id="copy-button"
-                    />
+    return (
+        <div className="sd-display-flex-column" data-test-id={props['data-test-id']}>
+            {props.label && <div className="sd-input__label">{props.label}</div>}
+            <div className="sd-d-flex gap-0-5">
+                <div className="sd-copyable-text-box">
+                    <span className="sd-copyable-text-box__text">{props.value}</span>
                 </div>
-                {this.props.helperText && <div className="sd-input__hint">{this.props.helperText}</div>}
+                <Button
+                    text={gettext('Copy')}
+                    icon={copied ? 'ok' : 'copy'}
+                    iconOnly={true}
+                    onClick={handleCopy}
+                    type="default"
+                    style="hollow"
+                    size={props.size ?? 'normal'}
+                    data-test-id="copy-button"
+                />
             </div>
-        );
-    }
-}
+            {props.helperText && <div className="sd-input__hint">{props.helperText}</div>}
+        </div>
+    );
+};
